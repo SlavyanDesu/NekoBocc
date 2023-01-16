359705dcf73a8d0ff818c607568f200c2b6d60b0
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2023-01-17 06:29:11 +0700

    Rusak coek

--- util/shared.ts
+++ util/shared.ts
@@ -5,7 +5,7 @@ export const header: {} = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
   }
 }
-export const endpoints: {} = {
+export const endpoints = {
   latest: '/category/hentai/page/__PAGE',
   search: '/search/__QUERY',
   random: '/random'
